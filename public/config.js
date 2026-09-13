57171ca4c3d52ab541f5f139bb74d31d53cef478
derenzhangyou18-rgb <derenzhangyou18@gmail.com>
2026-09-14 04:32:40 +0900

    Delete public directory

--- public/config.js
+++
@@ -1,6 +0,0 @@
-// このファイルにSupabaseのURLとPublishable keyを入れてください。
-// ※ service_role key / secret key は絶対にここへ入れないでください。
-window.SUPABASE_CONFIG = {
-  url: "https://nprkjamiczjpejhcngek.supabase.co",
-  key: "sb_publishable_PMe5CwuhpjKQhrj2dXGgUw_lMmPBBkW"
-};
